fde9171b8db7acbd7d925a9be9f72322d9904b5c
Sam Bowden <sam.bowden@techex.co.uk>
2025-07-03 17:16:15 +0100

    chore: clean up dist

--- dist/src/Utils/AudioUtils/UpdateStats.d.ts
+++
@@ -1,4 +0,0 @@
-import { type InputParams, type MixerParams } from '../../Types/ParamTypes';
-import { type ModifiedDataView } from '../../ModifiedDataView/ModifiedDataView';
-import type Stats from '../Stats';
-export declare function updateStats(audioData: ModifiedDataView, params: InputParams | MixerParams, stats: Stats): void;
