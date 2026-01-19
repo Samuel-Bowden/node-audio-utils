54ec297cf0a8a25a1d4b7881cc6bae4184285147
DanielHarris01 <daniel.harris@techex.co.uk>
2026-01-19 15:00:46 +0000

    feat: all tests passing

--- src/AudioStats/Loudness/LoudnessStats.test.ts
+++ src/AudioStats/Loudness/LoudnessStats.test.ts
@@ -2,43 +2,67 @@ import 'mocha';
 import {ChannelPosition, type ChannelsInputParams, LoudnessStatsWritable} from './LoudnessStats';
 import {expect} from 'chai';
 
-function * generateWave(numSamples: number, sampleRate: number, frequency: number) {
-	for (let i = 0; i < numSamples; i++) {
-		const t = i / sampleRate;
-		const theta = 2 * Math.PI * frequency * t;
-		const x = Math.sin(theta);
-		yield x;
-	}
-}
-
-function * repeatInner(generator: Generator<number>, n: number) {
-	for (const x of generator) {
-		for (let i = 0; i < n; i++) {
-			yield x;
-		}
-	}
-}
-
-function writeSingleSampleChunk(stats: LoudnessStatsWritable, sample: number) {
-	// Convert arbitrary int to a Uint8Array with 16 bit depth
-	const buffer = new ArrayBuffer(2);
-	const view = new DataView(buffer);
-	view.setUint16(0, ((2 ** 16) - 1) * (sample + 1) / 2, true);
-	stats._write(new Uint8Array(buffer), 'binary', () => undefined);
-}
-
 type WaveParams = {
 	durationMs: number;
 	numChannels?: number;
 	frequency?: number;
 	sampleRate?: number;
-	dBFS?: number;
+	dBFS?: number | number[];
 };
+
 function sendWave(stats: LoudnessStatsWritable, params: WaveParams) {
 	const sampleRate = params.sampleRate ?? 48_000;
-	for (const x of repeatInner(generateWave(sampleRate * params.durationMs / 1000, sampleRate, params.frequency ?? 1_000), params.numChannels ?? 2)) {
-		writeSingleSampleChunk(stats, x * dBFSToAmplitude(params.dBFS ?? 0));
+	const numChannels = params.numChannels ?? 2;
+	const frequency = params.frequency ?? 1_000;
+	const numSamples = Math.floor(sampleRate * params.durationMs / 1000);
+
+	let levelPerChannel: number[] = new Array<number>(numChannels);
+	if (Array.isArray(params.dBFS)) {
+		levelPerChannel = params.dBFS;
+	} else {
+		levelPerChannel.fill(params.dBFS ?? 0);
 	}
+
+	const amplitudePerChannel = levelPerChannel.map(dBFSToAmplitude);
+
+	// Send all samples as one buffer (unrealistic, but speeds up the tests)
+	const totalSamples = numSamples * numChannels;
+	const buffer = new ArrayBuffer(totalSamples * 2); // 16 bit
+	const view = new DataView(buffer);
+
+	let offset = 0;
+	for (let i = 0; i < numSamples; i++) {
+		const theta = 2 * Math.PI * i * frequency / sampleRate; // Samples * (waves / second) % (samples / second) = waves
+		for (let ch = 0; ch < numChannels; ch++) {
+			const sample = Math.sin(theta) * amplitudePerChannel[ch];
+			const uint16Value = ((2 ** 16) - 1) * (sample + 1) / 2;
+			view.setUint16(offset, uint16Value, true);
+			offset += 2;
+		}
+	}
+
+	stats._write(new Uint8Array(buffer), 'binary', () => undefined);
+}
+
+function sendSilence(stats: LoudnessStatsWritable, params: Omit<WaveParams, 'frequency' | 'dBFS'>) {
+	const sampleRate = params.sampleRate ?? 48_000;
+	const numChannels = params.numChannels ?? 2;
+	const numSamples = Math.floor(sampleRate * params.durationMs / 1000);
+
+	const totalSamples = numSamples * numChannels;
+	const buffer = new ArrayBuffer(totalSamples * 2);
+	const view = new DataView(buffer);
+
+	let offset = 0;
+	const uint16Value = ((2 ** 16) - 1) / 2;
+	for (let i = 0; i < numSamples; i++) {
+		for (let ch = 0; ch < numChannels; ch++) {
+			view.setUint16(offset, uint16Value, true);
+			offset += 2;
+		}
+	}
+
+	stats._write(new Uint8Array(buffer), 'binary', () => undefined);
 }
 
 function dBFSToAmplitude(dBFS: number): number {
@@ -56,6 +80,8 @@ function loudnessStatsParams(channels: ChannelPosition[]): ChannelsInputParams {
 }
 
 describe('LoudnessStats', () => {
+	// See https://www.itu.int/dms_pubrec/itu-r/rec/bs/R-REC-BS.1770-5-202311-I!!PDF-E.pdf
+
 	it('Passes ITU-R 1770 997Hz 0dBFS tone test', () => {
 		const loudnessStats = new LoudnessStatsWritable(
 			loudnessStatsParams([ChannelPosition.Centre]),
@@ -66,8 +92,10 @@ describe('LoudnessStats', () => {
 			frequency: 997,
 			durationMs: 20_000,
 		});
-		expect(loudnessStats.integratedLUFS).to.be.approximately(-3.01, 0.001);
+		expect(loudnessStats.I_LUFS).to.be.approximately(-3.01, 0.001);
 	});
+
+	// See https://tech.ebu.ch/docs/tech/tech3341.pdf
 
 	it('Passes EBU Test case #1', () => {
 		const loudnessStats = new LoudnessStatsWritable(
@@ -78,10 +106,10 @@ describe('LoudnessStats', () => {
 			durationMs: 20_000,
 			dBFS: -23.0,
 		});
-		expect(loudnessStats.momentaryLUFS).to.be.approximately(-23.0, 0.1);
-		expect(loudnessStats.shortTermLUFS).to.be.approximately(-23.0, 0.1);
-		expect(loudnessStats.integratedLUFS).to.be.approximately(-23.0, 0.1);
-	}).timeout(4_000);
+		expect(loudnessStats.M_LUFS).to.be.approximately(-23.0, 0.1);
+		expect(loudnessStats.S_LUFS).to.be.approximately(-23.0, 0.1);
+		expect(loudnessStats.I_LUFS).to.be.approximately(-23.0, 0.1);
+	});
 
 	it('Passes EBU Test case #2', () => {
 		const loudnessStats = new LoudnessStatsWritable(
@@ -92,10 +120,10 @@ describe('LoudnessStats', () => {
 			durationMs: 20_000,
 			dBFS: -33.0,
 		});
-		expect(loudnessStats.momentaryLUFS).to.be.approximately(-33.0, 0.1);
-		expect(loudnessStats.shortTermLUFS).to.be.approximately(-33.0, 0.1);
-		expect(loudnessStats.integratedLUFS).to.be.approximately(-33.0, 0.1);
-	}).timeout(4_000);
+		expect(loudnessStats.M_LUFS).to.be.approximately(-33.0, 0.1);
+		expect(loudnessStats.S_LUFS).to.be.approximately(-33.0, 0.1);
+		expect(loudnessStats.I_LUFS).to.be.approximately(-33.0, 0.1);
+	});
 
 	it('Passes EBU Test case #3', () => {
 		const loudnessStats = new LoudnessStatsWritable(
@@ -114,6 +142,130 @@ describe('LoudnessStats', () => {
 			durationMs: 10_000,
 			dBFS: -36.0,
 		});
-		expect(loudnessStats.integratedLUFS).to.be.approximately(-23.0, 0.1);
-	}).timeout(16_000);
+		expect(loudnessStats.I_LUFS).to.be.approximately(-23.0, 0.1);
+	}).timeout(4_000);
+
+	it('Passes EBU Test case #4', () => {
+		const loudnessStats = new LoudnessStatsWritable(
+			loudnessStatsParams([ChannelPosition.Left, ChannelPosition.Right]),
+			100_000,
+		);
+		sendWave(loudnessStats, {
+			durationMs: 10_000,
+			dBFS: -72.0,
+		});
+		sendWave(loudnessStats, {
+			durationMs: 10_000,
+			dBFS: -36.0,
+		});
+		sendWave(loudnessStats, {
+			durationMs: 60_000,
+			dBFS: -23.0,
+		});
+		sendWave(loudnessStats, {
+			durationMs: 10_000,
+			dBFS: -36.0,
+		});
+		sendWave(loudnessStats, {
+			durationMs: 10_000,
+			dBFS: -72.0,
+		});
+		expect(loudnessStats.I_LUFS).to.be.approximately(-23.0, 0.1);
+	}).timeout(4_000);
+
+	it('Passes EBU Test case #5', () => {
+		const loudnessStats = new LoudnessStatsWritable(
+			loudnessStatsParams([ChannelPosition.Left, ChannelPosition.Right]),
+			60_100,
+		);
+		sendWave(loudnessStats, {
+			durationMs: 20_000,
+			dBFS: -26.0,
+		});
+		sendWave(loudnessStats, {
+			durationMs: 20_100,
+			dBFS: -20.0,
+		});
+		sendWave(loudnessStats, {
+			durationMs: 20_000,
+			dBFS: -26.0,
+		});
+		expect(loudnessStats.I_LUFS).to.be.approximately(-23.0, 0.1);
+	}).timeout(4_000);
+
+	it('Passes EBU Test case #6', () => {
+		const loudnessStats = new LoudnessStatsWritable(
+			loudnessStatsParams([ChannelPosition.Left, ChannelPosition.Right, ChannelPosition.Centre, ChannelPosition.LeftSurround, ChannelPosition.RightSurround]),
+			20_000,
+		);
+		sendWave(loudnessStats, {
+			durationMs: 20_000,
+			numChannels: 5,
+			dBFS: [-28.0, -28.0, -24.0, -30.0, -30.0],
+		});
+		expect(loudnessStats.I_LUFS).to.be.approximately(-23.0, 0.1);
+	});
+
+	// #7 & #8 specify authentic programmes
+
+	it('Passes EBU Test case #9', () => {
+		const loudnessStats = new LoudnessStatsWritable(
+			loudnessStatsParams([ChannelPosition.Left, ChannelPosition.Right]),
+			20_000,
+		);
+		for (let i = 0; i < 3; i++) {
+			sendWave(loudnessStats, {durationMs: 1_340, dBFS: -20.0});
+			sendWave(loudnessStats, {durationMs: 1_660, dBFS: -30.0});
+		}
+
+		// S LUFS stabilises after 3s
+		expect(loudnessStats.S_LUFS).to.be.approximately(-23.0, 0.1);
+		for (let i = 0; i < 2; i++) {
+			sendWave(loudnessStats, {durationMs: 1_340, dBFS: -20.0});
+			expect(loudnessStats.S_LUFS).to.be.approximately(-23.0, 0.1);
+			sendWave(loudnessStats, {durationMs: 1_660, dBFS: -30.0});
+			expect(loudnessStats.S_LUFS).to.be.approximately(-23.0, 0.1);
+		}
+	});
+
+	// #10 is for file-based meters
+
+	it('Passes EBU Test case #11', () => {
+		const loudnessStats = new LoudnessStatsWritable(
+			loudnessStatsParams([ChannelPosition.Left, ChannelPosition.Right]),
+			20_000,
+		);
+		for (let i = 0; i < 20; i++) {
+			sendSilence(loudnessStats, {durationMs: i * 150});
+			sendWave(loudnessStats, {durationMs: 3_000, dBFS: i - 38.0});
+			sendSilence(loudnessStats, {durationMs: 3_000 - (i * 150)});
+
+			expect(loudnessStats.maxS_LUFS).to.be.approximately(i - 38.0, 0.1);
+		}
+	}).timeout(4_000);
+
+	it('Passes EBU Test case #12', () => {
+		const loudnessStats = new LoudnessStatsWritable(
+			loudnessStatsParams([ChannelPosition.Left, ChannelPosition.Right]),
+			20_000,
+		);
+		for (let i = 0; i < 3; i++) {
+			sendWave(loudnessStats, {durationMs: 180, dBFS: -20.0});
+			sendWave(loudnessStats, {durationMs: 220, dBFS: -30.0});
+		}
+
+		// M LUFS stabilises after 1s
+		expect(loudnessStats.M_LUFS).to.be.approximately(-23.0, 0.1);
+		for (let i = 0; i < 22; i++) {
+			sendWave(loudnessStats, {durationMs: 180, dBFS: -20.0});
+			sendWave(loudnessStats, {durationMs: 220, dBFS: -30.0});
+			expect(loudnessStats.M_LUFS).to.be.approximately(-23.0, 0.1);
+		}
+	});
+
+	// #13 is for file-based meters
+
+	// #14 is impossible under the recommended 100ms window step size for momentary loudness
+	// See: https://github.com/jiixyj/libebur128/issues/93
+	// To pass this would require a 20ms step size
 });
--- src/AudioStats/Loudness/LoudnessStats.ts
+++ src/AudioStats/Loudness/LoudnessStats.ts
@@ -9,8 +9,6 @@ import {KFilter} from './KFilter';
 
 /** Extracts samples from audio chunk according to bitDepth, endianness, etc. */
 abstract class AbstractWritable extends Writable {
-	/** Expected channel index of the next sample to be received */
-	private currentChannel = 0;
 	constructor(readonly params: ChannelsInputParams) {
 		super();
 	}
@@ -23,23 +21,33 @@ abstract class AbstractWritable extends Writable {
 		const getSampleMethod: `get${IntType}${BitDepth}` = `get${getMethodName(this.params.bitDepth, this.params.unsigned)}`;
 
 		for (let index = 0; index < audioData.byteLength; index += bytesPerElement) {
-			const sample = audioData[getSampleMethod](index, isLe);
-			this.onSample(this.currentChannel, sample);
-			this.currentChannel = (this.currentChannel + 1) % this.params.channels.length;
+			let sample = audioData[getSampleMethod](index, isLe);
+
+			// Normalise to [-1,1]
+			// ~TODO: account for signed input
+			sample = 2 * ((sample / (2 ** this.params.bitDepth)) - 0.5);
+
+			this.onSample(sample);
 		}
 
 		callback();
 	}
 
-	abstract onSample(channel: number, sample: number): void;
+	abstract onSample(sample: number): void;
 }
 
 /** Annex 1: https://www.itu.int/dms_pubrec/itu-r/rec/bs/R-REC-BS.1770-5-202311-I!!PDF-E.pdf */
 export class LoudnessStatsWritable extends AbstractWritable {
 	// Stats:
-	public momentaryLUFS = 0;
-	public shortTermLUFS = 0;
-	public integratedLUFS = 0;
+	public M_LUFS = 0;
+	public S_LUFS = 0;
+	public I_LUFS = 0;
+
+	public maxM_LUFS = -Infinity;
+	public maxS_LUFS = -Infinity;
+
+	/** Expected channel index of the next sample to be received */
+	private currentChannel = 0;
 
 	private readonly kFilters: KFilter[];
 
@@ -55,26 +63,25 @@ export class LoudnessStatsWritable extends AbstractWritable {
 		super(params);
 		this.kFilters = this.params.channels.map(() => new KFilter());
 
-		this.blockAccumulator3s = new BlockAccumulator(params.sampleRate, params.channels, 3000);
-		this.blockAccumulator400ms = new BlockAccumulator(params.sampleRate, params.channels, 400);
+		this.blockAccumulator3s = new BlockAccumulator(params.sampleRate, params.channels, 100, 3_000);
+		this.blockAccumulator400ms = new BlockAccumulator(params.sampleRate, params.channels, 100, 400);
 
-		this.maxGatingBlocksBufferSize = integratedMeasurementIntervalMs / 400;
+		this.maxGatingBlocksBufferSize = integratedMeasurementIntervalMs / 100; // New block produced every 100ms
 	}
 
-	onSample(channel: number, x: number): void {
-		// Normalise
-		// ~TODO: account for signed
-		x = 2 * ((x / (2 ** this.params.bitDepth)) - 0.5);
-		const y = this.kFilters[channel].onSample(x);
+	onSample(x: number): void {
+		const y = this.kFilters[this.currentChannel].onSample(x);
 
-		const block3s = this.blockAccumulator3s.addSample(channel, y);
+		const block3s = this.blockAccumulator3s.addSample(y);
 		if (block3s) {
-			this.shortTermLUFS = block3s.loudness;
+			this.S_LUFS = block3s.loudness;
+			this.maxS_LUFS = Math.max(this.maxS_LUFS, this.S_LUFS);
 		}
 
-		const block400ms = this.blockAccumulator400ms.addSample(channel, y);
+		const block400ms = this.blockAccumulator400ms.addSample(y);
 		if (block400ms) {
-			this.momentaryLUFS = block400ms.loudness;
+			this.M_LUFS = block400ms.loudness;
+			this.maxM_LUFS = Math.max(this.maxM_LUFS, this.M_LUFS);
 
 			this.gatingBlocks.push(block400ms);
 			if (this.gatingBlocks.length > this.maxGatingBlocksBufferSize) {
@@ -83,8 +90,10 @@ export class LoudnessStatsWritable extends AbstractWritable {
 
 			const absoluteThreshold = -70;
 			const relativeThreshold = gatedLoudness(this.gatingBlocks, absoluteThreshold) - 10;
-			this.integratedLUFS = gatedLoudness(this.gatingBlocks, Math.max(absoluteThreshold, relativeThreshold));
+			this.I_LUFS = gatedLoudness(this.gatingBlocks, Math.max(absoluteThreshold, relativeThreshold));
 		}
+
+		this.currentChannel = (this.currentChannel + 1) % this.params.channels.length;
 	}
 }
 
@@ -114,35 +123,53 @@ class Block {
 }
 
 /** Accumulates a duration of samples.
+ *  Blocks overlap by 75% of their duration
  *  Momentary and Short-Term loudnesses are simply the loudness of a window of K-Filtered samples of duration 0.4 and 3s respectively.
- *  Integrated loudness accumulates 0.4s "blocks" and applies gating.
 */
 class BlockAccumulator {
 	/** Buffer[channelIdx][sampleIdx] */
-	private buffer: number[][] = [];
+	private readonly ringbuffer: number[][] = [];
+	private ptr = 0;
+	/** The number of samples (per channel) remaining to collect until we can emit a new block
+	 *  Initially this is the size of the buffer, but subsequent blocks only collect 25% new samples.
+	*/
+	private samplesUntillFull: number;
 
-	constructor(readonly sampleRate: SampleRate, readonly channelPositions: ChannelPosition[], readonly targetDurationMs = 400) {
-		this.clearBuffer();
+	private readonly bufferSize: number;
+	private readonly stepSize: number;
+	private readonly numChannels: number;
+
+	private currentChannel = 0;
+
+	constructor(readonly sampleRate: SampleRate, readonly channelPositions: ChannelPosition[], stepMs: number, readonly targetDurationMs: number) {
+		this.bufferSize = Math.ceil(sampleRate * targetDurationMs / 1000);
+		this.stepSize = Math.ceil(sampleRate * stepMs / 1000);
+
+		this.numChannels = channelPositions.length;
+
+		this.ringbuffer = channelPositions.map(() => new Array<number>(this.bufferSize));
+		this.samplesUntillFull = this.bufferSize;
 	}
 
-	addSample(channel: number, sample: number): Block | undefined {
-		this.buffer[channel].push(sample);
-		// Upon adding the final channel our buffers should have the same length since samples for each channel are added in sequence
-		const lastChannelIdx = this.channelPositions.length - 1;
-		if (channel === lastChannelIdx) {
-			const bufferDuration = this.buffer[lastChannelIdx].length / this.sampleRate;
-			if (bufferDuration > this.targetDurationMs / 1000) {
-				const block = new Block(this.buffer, this.channelPositions);
-				this.clearBuffer();
-				return block;
+	addSample(sample: number): Block | undefined {
+		this.ringbuffer[this.currentChannel][this.ptr] = sample;
+
+		let block;
+
+		// Upon adding the final channel we increment the pointer
+		if (this.currentChannel === this.channelPositions.length - 1) {
+			this.samplesUntillFull -= 1;
+			if (this.samplesUntillFull === 0) {
+				block = new Block(this.ringbuffer, this.channelPositions);
+				this.samplesUntillFull = this.stepSize;
 			}
+
+			this.ptr = (this.ptr + 1) % this.bufferSize;
 		}
 
-		return undefined;
-	}
+		this.currentChannel = (this.currentChannel + 1) % this.numChannels;
 
-	private clearBuffer() {
-		this.buffer = this.channelPositions.map(() => []);
+		return block;
 	}
 }
 
